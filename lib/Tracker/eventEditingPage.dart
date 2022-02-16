489bb2d857a3c49f87fecc8d521da9f7c14632a0
adtoria <adityyyyadav@gmail.com>
2022-02-17 04:47:10 +0530

    Added tracker: events editing page

--- lib/Tracker/eventEditingPage.dart
+++ lib/Tracker/eventEditingPage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'event.dart';
 import 'utils.dart';
 
@@ -85,16 +84,138 @@ class _EventEditingPageState extends State<EventEditingPage> {
   Widget buildDateTimePickers() => Column(
         children: [
           buildFrom(),
+          buildTo(),
         ],
       );
 
-  Widget buildFrom() => Row(
+  Widget buildFrom() => buildHeader(
+        header: 'FROM',
+        child: Row(
+          children: [
+            Expanded(
+              flex: 2,
+              child: buildDropdownField(
+                text: Utils.toDate(fromDate),
+                onClicked: () => pickFromDateTime(pickDate: true),
+              ),
+            ),
+            Expanded(
+              child: buildDropdownField(
+                text: Utils.toTime(fromDate),
+                onClicked: () => pickFromDateTime(pickDate: false),
+              ),
+            ),
+          ],
+        ),
+      );
+
+  Widget buildTo() => buildHeader(
+        header: 'TO',
+        child: Row(
+          children: [
+            Expanded(
+              flex: 2,
+              child: buildDropdownField(
+                text: Utils.toDate(toDate),
+                onClicked: () {},
+              ),
+            ),
+            Expanded(
+              child: buildDropdownField(
+                text: Utils.toTime(toDate),
+                onClicked: () {},
+              ),
+            ),
+          ],
+        ),
+      );
+
+  Future pickFromDateTime({required bool pickDate}) async {
+    final date = await pickDateTime(fromDate, pickDate: pickDate);
+    if (date == null) return;
+
+    if (date.isAfter(toDate)) {
+      toDate =
+          DateTime(date.year, date.month, date.day, toDate.hour, toDate.minute);
+    }
+
+    setState(() => fromDate = date);
+  }
+
+  Future pickToDateTime({required bool pickDate}) async {
+    final date = await pickDateTime(
+      toDate,
+      pickDate: pickDate,
+      firstDate: pickDate ? fromDate : null,
+    );
+    if (date == null) return;
+
+    if (date.isAfter(toDate)) {
+      toDate =
+          DateTime(date.year, date.month, date.day, toDate.hour, toDate.minute);
+    }
+
+    setState(() => toDate = date);
+  }
+
+  Future<DateTime?> pickDateTime(
+    DateTime initialDate, {
+    required bool pickDate,
+    DateTime? firstDate,
+  }) async {
+    if (pickDate) {
+      final date = await showDatePicker(
+        context: context,
+        initialDate: initialDate,
+        firstDate: firstDate ?? DateTime(2015, 8),
+        lastDate: DateTime(2101),
+      );
+
+      if (date == null) return null;
+
+      final time = Duration(
+        hours: initialDate.hour,
+        minutes: initialDate.minute,
+      );
+
+      return date.add(time);
+    } else {
+      final timeOfDay = await showTimePicker(
+        context: context,
+        initialTime: TimeOfDay.fromDateTime(initialDate),
+      );
+
+      if (timeOfDay == null) return null;
+
+      final date =
+          DateTime(initialDate.year, initialDate.month, initialDate.day);
+      final time = Duration(hours: timeOfDay.hour, minutes: timeOfDay.minute);
+
+      return date.add(time);
+    }
+  }
+
+  Widget buildDropdownField({
+    required String text,
+    required VoidCallback onClicked,
+  }) =>
+      ListTile(
+        title: Text(text),
+        trailing: Icon(Icons.arrow_drop_down),
+        onTap: onClicked,
+      );
+
+  Widget buildHeader({
+    required String header,
+    required Widget child,
+  }) =>
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          // Expanded(
-          //   child: buildDropdownField(
-          //     text: Utils.toDate(fromDate),
-          //   ),
-          // ),
+          Text(
+            header,
+            style: TextStyle(fontWeight: FontWeight.bold),
+          )
         ],
       );
 }
--- lib/Tracker/utils.dart
+++ lib/Tracker/utils.dart
@@ -1,9 +1,15 @@
 import 'package:intl/intl.dart';
 
 class Utils {
-  String toDate(DateTime dateTime) {
+  static String toDate(DateTime dateTime) {
     final date = DateFormat.yMMMEd().format(dateTime);
 
     return '$date';
+  }
+
+  static String toTime(DateTime dateTime) {
+    final time = DateFormat.Hm().format(dateTime);
+
+    return '$time';
   }
 }
