0f518df2ce145c5ed1c1981abcb8be6cdd891645
skh1203 <shivamharjani1203@gmail.com>
2022-03-23 16:34:43 +0530

    Added snackbar

--- lib/LoginRegister/forgotPassword.dart
+++ lib/LoginRegister/forgotPassword.dart
@@ -10,23 +10,28 @@ class ForgotPassword extends StatefulWidget {
 }
 
 class _ForgotPasswordState extends State<ForgotPassword> {
-  final formkey=GlobalKey<FormState>();
-  final emailController=TextEditingController();
+  final formkey = GlobalKey<FormState>();
+  final emailController = TextEditingController();
 
   Future resetPassword() async {
-    showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (context) => Center(child: CircularProgressIndicator(),),
-    );
 
-    try{
-      await FirebaseAuth.instance.sendPasswordResetEmail(email: emailController.text.trim());
-      Navigator.of(context).popUntil((route) => route.isFirst);
-    }
-    on FirebaseAuthException catch (e){
+    try {
+      await FirebaseAuth.instance
+          .sendPasswordResetEmail(email: emailController.text.trim());
+      final snackbar = SnackBar(
+        content: Text("Password reset mail sent successfully"),
+        duration: Duration(seconds: 5),
+      );
+
+      ScaffoldMessenger.of(context).showSnackBar(snackbar);
+    } on FirebaseAuthException catch (e) {
       print(e);
-      Navigator.of(context).pop();
+      final snackbar = SnackBar(
+        content: Text("An error occured"),
+        duration: Duration(seconds: 5),
+      );
+
+      ScaffoldMessenger.of(context).showSnackBar(snackbar);
     }
   }
 
@@ -40,77 +45,76 @@ class _ForgotPasswordState extends State<ForgotPassword> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Color(0xFF1E233C),
-        title: Text("Reset Password"),
-      ),
-      
-      body: Padding(
-        padding: EdgeInsets.all(16),
-        child: Form(
-          key: formkey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                color: Color(0xFF9F86C0),
-                child: Padding(
-                  padding: EdgeInsetsDirectional.fromSTEB(8, 8, 8, 8),
-                  child: Text("Enter your email address that you used to register. "
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Color(0xFF1E233C),
+          title: Text("Reset Password"),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(16),
+          child: Form(
+            key: formkey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  color: Color(0xFF9F86C0),
+                  child: Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(8, 8, 8, 8),
+                    child: Text(
+                      "Enter your email address that you used to register. "
                       "We'll send you an email with a link to reset your password.",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 17,
-                      fontWeight: FontWeight.w700
+                      textAlign: TextAlign.center,
+                      style:
+                          TextStyle(fontSize: 17, fontWeight: FontWeight.w700),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(height: 20.0,),
-              TextFormField(
-                keyboardType: TextInputType.emailAddress,
-                controller: emailController,
-                cursorColor: Colors.white,
-                textInputAction: TextInputAction.done,
-                decoration: InputDecoration(
-                  labelText: "Email",
+                SizedBox(
+                  height: 20.0,
                 ),
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                validator: (value) {
-                  if (value == null) {
-                    return "* Required";
-                  } else if (!RegExp(
-                      "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]")
-                      .hasMatch(value)) {
-                    return 'Please enter a valid email';
-                  } else {
-                    return null;
-                  }
-                }
-              ),
-              SizedBox(height: 45.0,),
-              SizedBox(
-                width: 155,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: Size.fromHeight(50),
-                    primary: Color(0xFF1E233C),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20)
-                    )
-                  ),
-                  //icon: Icon(Icons.email_outlined),
-                  child: Text("Send",
-                    style: TextStyle(fontSize: 24),
-                  ),
-                  onPressed: resetPassword,
+                TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    controller: emailController,
+                    cursorColor: Colors.white,
+                    textInputAction: TextInputAction.done,
+                    decoration: InputDecoration(
+                      labelText: "Email",
+                    ),
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    validator: (value) {
+                      if (value == null) {
+                        return "* Required";
+                      } else if (!RegExp(
+                              "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]")
+                          .hasMatch(value)) {
+                        return 'Please enter a valid email';
+                      } else {
+                        return null;
+                      }
+                    }),
+                SizedBox(
+                  height: 45.0,
                 ),
-              )
-            ],
+                SizedBox(
+                  width: 155,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        minimumSize: Size.fromHeight(50),
+                        primary: Color(0xFF1E233C),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20))),
+                    //icon: Icon(Icons.email_outlined),
+                    child: Text(
+                      "Send",
+                      style: TextStyle(fontSize: 24),
+                    ),
+                    onPressed: resetPassword,
+                  ),
+                )
+              ],
+            ),
           ),
-        ),
-      )
-    );
+        ));
   }
 }
